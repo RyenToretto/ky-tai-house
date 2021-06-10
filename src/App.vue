002badf347fb2b4d90e5b49ccd81462b1dfe06f1
koujianfeng <koujianfeng@do-global.com>
2021-06-10 16:33:34 +0800

    feat: custom #KJF-20210610

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
       </div>
     </header>
+
     <main>
-      <router-view/>
+      <router-view></router-view>
     </main>
   </div>
 </template>
